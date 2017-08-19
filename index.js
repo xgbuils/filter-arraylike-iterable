70eea328b91d9f2f4ea51c09b39eb04f2e572226
Xavier Garcia Buils <xgbuils@gmail.com>
2017-08-19 14:13:58 +0200

    use own implementation of every

--- index.js
+++ index.js
@@ -10,6 +10,16 @@ function FilterArrayLikeIterable (iterable) {
     this.ps = []
 }
 
+function every (p, arr) {
+    const length = arr.length
+    for (let i = 0; i < length; ++i) {
+        if (!p(arr[i])) {
+            return false
+        }
+    }
+    return true
+}
+
 Object.defineProperties(FilterArrayLikeIterable.prototype, {
     filter: {
         value: filter
@@ -20,7 +30,7 @@ Object.defineProperties(FilterArrayLikeIterable.prototype, {
             const length = iterable.length
             for (let i = 0; i < length; ++i) {
                 const val = iterable[i]
-                if (this.ps.every(p => p(val))) {
+                if (every(p => p(val), this.ps)) {
                     yield val
                 }
             }
